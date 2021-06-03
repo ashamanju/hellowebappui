56c9422e62499f37374c1d7c5547f17e1e6872cf
Manju <asha.manju@finastra.com>
2021-06-03 23:50:22 +0530

    Initial commit

---
+++ src/main/java/com/az204/hellowebappui/HellowebappuiApplication.java
@@ -0,0 +1,13 @@
+package com.az204.hellowebappui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HellowebappuiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HellowebappuiApplication.class, args);
+    }
+
+}
